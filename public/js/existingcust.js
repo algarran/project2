6c2c9ccc276da240a902c3032132a5a02bd683be
Spencer Wood <spencer.wood@me.com>
2020-03-25 20:51:22 -0500

    completed the existingcust.js file call, error in that needs fixed

--- public/js/existingcust.js
+++ public/js/existingcust.js
@@ -8,7 +8,10 @@ $.get('/api/all', function(data) {
 
 
         //all the customer info returned
-        
+        $('#customer-exist-' + i).append(`<h2>Customer Info:First Name:${data[i].firstName}</h2>`);
+        $('#customer-exist-' + i).append(`<h2>Last Name:${data[i].lastName}</h2>`);
+        $('#customer-exist-' + i).append(`<h6>Phone Number:${data[i].phone}</h6>`);
+        $('#customer-exist-' + i).append(`<h6>Email:${data[i].email}</h6>`);
 
     }
 });
