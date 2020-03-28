21bf7b5e14d034d3d56b1d1e7f4014fb75a567e0
Spencer Wood <spencer.wood@me.com>
2020-03-28 09:52:52 -0500

    created new sequelize files for client, job, address and uncommented out the body background image

---
+++ models/address.js
@@ -0,0 +1,23 @@
+// Creating our User model
+module.exports = function(sequelize, DataTypes) {
+  var Address = sequelize.define("Address", {
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    county: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+  });
+  return Address
+};
